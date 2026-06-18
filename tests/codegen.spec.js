6b4a824c7aeed14e0436be45332a5920f5ba18ea
Arshad24S <yaklur66@gmail.com>
2026-06-17 17:04:12 -0700

    "new changes"

--- tests/codegen.spec.js
+++ tests/codegen.spec.js
@@ -1,26 +1,90 @@
 const { test, expect } = require('@playwright/test');
 
-test('codegen test',{tag:['@abc','@dcb']}, async ({ page }) => {
+test('codegen test', { tag: ['@abc', '@dcb'] }, async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-     await page.locator('label').filter({ hasText: 'Radio1' }).getByRole('radio').check();
-      await page.locator('label').filter({ hasText: 'Radio1' }).getByRole('radio').isChecked();
-       await page.locator("//input[@id='autocomplete']").click();
-      await page.locator("//input[@id='autocomplete']").pressSequentially('ind');
-      const suggestions = await page.locator("#ui-id-1")
-      await suggestions.filter({ hasText: 'India' }).click();
-      await page.locator("#dropdown-class-example").selectOption('option2');
-      await page.locator("//input[@id='checkBoxOption1']").click();
+    await page.locator('label').filter({ hasText: 'Radio1' }).getByRole('radio').check();
+    await page.locator('label').filter({ hasText: 'Radio1' }).getByRole('radio').isChecked();
 
-      const tablerows= await page.locator(".table-display tr")
-      const tablecolumns= await page.locator(".table-display td")
-      for (let i=0;i< await tablerows.count();i++){
-        const rowtext= await tablerows.nth(i).textContent();
-        if(rowtext.includes("Python")){console.log(rowtext);}
+       page.on ('dialog', async dialog => {
+            console.log(dialog.message());
+            await dialog.accept();
+         });
+        await page.locator("#alertbtn").click();
 
-       await page.locator("#displayed-text").isVisible();
-       await page.locator("hide-textbox").click();
-       await page.locator("#displayed-text").isHidden();
+    
+    await page.locator("//input[@id='autocomplete']").click();
+    await page.locator("//input[@id='autocomplete']").pressSequentially('ind');
+    const suggestions = await page.locator("#ui-id-1")
+    await suggestions.filter({ hasText: 'India' }).click();
 
-      
+    await page.locator("#dropdown-class-example").selectOption('option2');
+    await page.locator("//input[@id='checkBoxOption1']").click();
+
+  
+
+    const tablerows = await page.locator(".table-display tr")
+    const tablecolumns = await page.locator(".table-display td")
+    for (let i = 0; i < await tablerows.count(); i++) {
+        const rowtext = await tablerows.nth(i).textContent();
+        if (rowtext.includes("Python")) {
+            console.log(rowtext);
+        }
     }
+
+    
+
+
+    await page.locator("#displayed-text").isVisible();
+    await page.locator("#hide-textbox").click();
+    await page.locator("#displayed-text").isHidden();
+
+    const headers = await page.locator(".tableFixHead thead tr th")
+
+    for (let i = 0; i < await headers.count(); i++) {
+        const headertext = await headers.nth(i).textContent();
+        console.log(headertext);
+    }
+
+     await page.locator("//input[@id='name']").click();
+        await page.locator("//input[@id='name']").fill("Arshad");
+
+        page.on ('dialog', async dialog => {
+         console.log(dialog.message());
+         });
+        await page.locator("#confirmbtn").click();
+
+    const rowdata = await page.locator(".tableFixHead tbody tr")
+    for (let i = 0; i < await rowdata.count(); i++) {
+        const rowdatatext = await rowdata.nth(i).textContent();
+        if (rowdatatext.includes("Bengaluru")) {
+            console.log(rowdatatext);
+        }
+    }
+
+       page.on ('dialog', async dialog => {
+            console.log(dialog.message());
+         });
+        await page.locator("#confirmbtn").click();
+
+        const openwindow = page.locator("#openwindow");
+
+        const [newpage] = await Promise.all([
+             context.waitForEvent('page'),
+             openwindow.click(),
+        ]);
+
+        await newpage.waitForLoadState();
+        const newpagetitle = await newpage.title();
+        console.log(newpagetitle);
+        await page.bringToFront();
+        await newpage.bringToFront();
+        newpage.close();
+    
+
+       
+        
 })
